03ee8ba9aac35e81205fb4ebc2fa76ca6a700d33
praveenpsn <54975936+praveenpsn@users.noreply.github.com>
2019-10-28 13:26:27 +0530

    Initial commit

---
+++ welcomeps/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Welcomeapp1Config(AppConfig):
+    name = 'welcomeps'
---
+++ welcomeps/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def praveen (requesrt):
+    return HttpResponse("<html><body bgcolor=blue>welcome to django mr praveeen</body></html>")
